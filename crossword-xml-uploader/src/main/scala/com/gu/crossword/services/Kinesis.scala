fd5dce3d6df4a5f9541673329e07f4519bc0c58f
Philip McMahon <philip.mcmahon@guardian.co.uk>
2016-02-02 15:57:21 +0000

    Remove profilecredentialsprovider.

--- crossword-xml-uploader/src/main/scala/com/gu/crossword/services/Kinesis.scala
+++ crossword-xml-uploader/src/main/scala/com/gu/crossword/services/Kinesis.scala
@@ -1,6 +1,5 @@
 package com.gu.crossword.services
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.regions.{ Region, Regions }
 import com.amazonaws.services.kinesis.AmazonKinesisAsyncClient
 import com.gu.crossword.Config
@@ -8,7 +7,7 @@ import com.gu.crossword.Config
 trait Kinesis {
 
   lazy val kinesisClient: AmazonKinesisAsyncClient = {
-    val kinesisClient = new AmazonKinesisAsyncClient(new ProfileCredentialsProvider("composer"))
+    val kinesisClient = new AmazonKinesisAsyncClient()
     kinesisClient.setRegion(Region.getRegion(Regions.EU_WEST_1))
     kinesisClient
 
