6c4ff0fd445ae52dc800b97abaf61dceba9e87f3
Robert Kenny <kenoir@gmail.com>
2023-09-11 15:42:49 +0100

    better print stack trace
    Co-authored-by: Andrew Nowak <andrew.nowak@guardian.co.uk>

--- crossword-xml-uploader/src/main/scala/com/gu/crossword/Lambda.scala
+++ crossword-xml-uploader/src/main/scala/com/gu/crossword/Lambda.scala
@@ -40,11 +40,11 @@ trait CrosswordUploaderLambda
     v2Result match {
       case Success(_) =>
         println(s"Successfully dual uploaded crossword ${crosswordXmlFile.key} to crosswordv2")
-      case Failure(error) =>
+      case Failure(e) =>
         println(
           s"Failed to dual upload crossword ${crosswordXmlFile.key} to crosswordv2 with error: ${error.getMessage}"
         )
-        error.getStackTrace.foreach(println)
+        e.printStackTrace()
     }
   }
 
