f8a36be7e63bab9e253897e6fd1059cfab0bced1
Robert Kenny <kenoir@gmail.com>
2023-09-11 14:58:22 +0100

    add dual running for pdf-uploader

--- crossword-pdf-uploader/src/main/scala/com/gu/crossword/pdfuploader/models/CrosswordPdfLambdaConfig.scala
+++ crossword-pdf-uploader/src/main/scala/com/gu/crossword/pdfuploader/models/CrosswordPdfLambdaConfig.scala
@@ -2,6 +2,7 @@ package com.gu.crossword.pdfuploader.models
 
 case class CrosswordPdfLambdaConfig(
   crosswordMicroAppUrl: String,
+  crosswordV2Url: String,
   crosswordsBucketName: String,
   crosswordPdfPublicBucketName: String,
   crosswordPdfPublicFileLocation: String,
