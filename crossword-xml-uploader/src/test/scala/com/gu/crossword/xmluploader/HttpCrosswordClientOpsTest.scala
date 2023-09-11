0fe8e662409b1b628378b076d85aaeee9ed7be73
Robert Kenny <kenoir@gmail.com>
2023-09-11 14:05:07 +0100

    further test xml-uploader contract with api

--- crossword-xml-uploader/src/test/scala/com/gu/crossword/xmluploader/HttpCrosswordClientOpsTest.scala
+++ crossword-xml-uploader/src/test/scala/com/gu/crossword/xmluploader/HttpCrosswordClientOpsTest.scala
@@ -1,10 +1,13 @@
 package com.gu.crossword.xmluploader
 
+import okhttp3.MultipartReader
 import okhttp3.mockwebserver.MockResponse
 import org.scalatest.TryValues
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import okhttp3.mockwebserver.MockWebServer
+
+import java.nio.charset.StandardCharsets
 
 
 class HttpCrosswordClientOpsTest extends AnyFlatSpec with Matchers with TryValues {
@@ -23,8 +26,29 @@ class HttpCrosswordClientOpsTest extends AnyFlatSpec with Matchers with TryValue
     val baseUrl = mockHttpServer.url("/upload").toString
     mockHttpServer.enqueue(new MockResponse().setBody(expectedResponse));
 
-    val result = httpCrosswordClientOps.upload(baseUrl)("id", Array.empty)
+    val encodedString = "hello world"
+    val sentData = encodedString.getBytes(StandardCharsets.UTF_8);
+    val result = httpCrosswordClientOps.upload(baseUrl)("id", sentData)
     result.get shouldBe expectedResponse
+
+    val request = mockHttpServer.takeRequest();
+    request.getPath should be("/upload")
+    request.getMethod should be("POST")
+    request.getHeader("Content-Type") should include("multipart/form-data; boundary=")
+
+    // parse multipart/form-data body
+    val boundary = request.getHeader("Content-Type").split("boundary=").last
+    val multipartReader = new MultipartReader(request.getBody, boundary)
+
+    val resultFormatPart = multipartReader.nextPart()
+    resultFormatPart.headers().get("Content-Disposition") should include("name=\"result_format\"")
+    resultFormatPart.body().readString(StandardCharsets.UTF_8) shouldBe "xml"
+
+    val fileData = multipartReader.nextPart()
+    fileData.headers().get("Content-Disposition") should include("name=\"xml\"; filename=\"id")
+    fileData.body().readString(StandardCharsets.UTF_8) shouldBe encodedString
+
+    multipartReader.nextPart() shouldBe null
 
     mockHttpServer.shutdown()
   }
